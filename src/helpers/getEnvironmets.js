eb0eb01d5b79c502b9205bae265ca122114e271c
lukitasojeda <lukitas.ojeda.00@gmail.com>
2022-08-15 17:24:10 -0300

    bug al hacer build 'import.meta' solucionado

--- src/helpers/getEnvironmets.js
+++ src/helpers/getEnvironmets.js
@@ -2,9 +2,15 @@
 
 export const getEnvironmets = () => {
 
-    import.meta.env;
+    // import.meta.env;
 
     return {
-        ...import.meta.env
+        VITE_FIREBASE_apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+        VITE_FIREBASE_authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+        VITE_FIREBASE_projectId: import.meta.env.VITE_FIREBASE_projectId,
+        VITE_FIREBASE_storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+        VITE_FIREBASE_messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+        VITE_FIREBASE_appId: import.meta.env.VITE_FIREBASE_appId,
+        VITE_FIREBASE_measurementId: import.meta.env.VITE_FIREBASE_measurementId,
     }
 } 
